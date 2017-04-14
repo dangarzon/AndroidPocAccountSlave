820ca113803d159262af1f7e6d8550f6a57d3298
Daniel Garzon <daniel.garzon.sanchez@gmail.com>
2017-04-14 22:58:45 +0200

    Show info from Account

--- app/src/main/java/arko/bones/com/accountslave/MainActivity.java
+++ app/src/main/java/arko/bones/com/accountslave/MainActivity.java
@@ -1,10 +1,14 @@
 package arko.bones.com.accountslave;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AccountManager accountManager;
 
     // UI references.
     private TextView mAccountView;
@@ -15,12 +19,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Store AccountManager
+        accountManager = AccountManager.get(this);
+
         // Locate References
         mAccountView = (TextView) findViewById(R.id.account_text_view);
         mUserView = (TextView) findViewById(R.id.user_text_view);
 
-        mAccountView.setText(R.string.info_no_account);
-        mUserView.setText(R.string.info_no_user);
+        showAccountInfo();
+    }
+
+    /**
+     * Get Usser Account
+     *
+     * @return user account
+     */
+    private Account getAccount() {
+        Account[] accounts = accountManager.getAccountsByType(getString(R.string.account_type));
+
+        Account account = null;
+        for (Account item: accounts) {
+            if (item.name.equals(getString(R.string.account_name))) {
+                account = item;
+            }
+        }
+
+        return account;
+    }
+
+    /**
+     * Show Account Info
+     */
+    private void showAccountInfo() {
+        Account account = getAccount();
+
+        if (null != account) {
+            mAccountView.setText(R.string.account_name);
+
+            String info = accountManager.getUserData(account, "user")
+                + ":"
+                + accountManager.getUserData(account, "pass");
+
+            mUserView.setText(info);
+        } else {
+            mAccountView.setText(R.string.info_no_account);
+            mUserView.setText(R.string.info_no_user);
+        }
     }
 
 }
